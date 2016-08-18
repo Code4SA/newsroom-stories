a4a9dae62c3403749036a6b5d6b0490d13a7bf65
lailahr <lailah@AncestreStudio.local>
2016-08-18 13:49:54 +0200

    fix baseurl to support static

--- embeds/embed-dom-houtbay.js
+++ embeds/embed-dom-houtbay.js
@@ -527,7 +527,7 @@ if (typeof(window.pym) === 'undefined') {
 if (document.location.hostname == "localhost") {
   var baseurl = "";
 } else {
-  var baseurl = "http://code4sa.org/newsroom-stories";
+  var baseurl = "https://static.code4sa.org/newsroom-stories";
 }
 
 var container = document.getElementById('/2016/08/04/houtbay');
